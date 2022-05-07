29fc1293ac2e6e25027acac82e2a03237b58b2ae
Chris Edosomwan <christopher.edosomwan@gmail.com>
2022-05-07 13:11:09 -0400

    finish Age-calculator

--- Age_Calculator.py
+++ Age_Calculator.py
@@ -1,8 +1,8 @@
-#Age Calculator Part 1
+#Age Calculator Part 2
 
 #Chris Edosomwan Freshman year CS 
 
-#April 29, 2022
+#May 7 2022
 
 import datetime
 
@@ -22,19 +22,21 @@ current_day = datetime.date.today().day
 
 age_year = current_year - birth_year
 
-age_month = current_month + birth_month
-
-age_day = current_day - birth_day
 
 
-print("your exact age ", age_year, "Years", age_month, 'months', age_day, "days")
-
-
-
-
-
-
-
-
-
+if current_month > birth_month:
+   age_month = current_month - birth_month
+   
+   
+if current_month < birth_month:
+    age_year = age_year - 1
+    age_month = birth_month - current_month
+    age_month = 12 - age_month
     
+    
+ 
+
+
+
+print("your exact age ", age_year, "Years", age_month, 'months',current_day, "days")
+
